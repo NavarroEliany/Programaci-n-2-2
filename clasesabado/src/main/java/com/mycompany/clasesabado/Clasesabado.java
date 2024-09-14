6533a74e8f420ff70d9f62ea0a871ccb0c958144
NavarroEliany <elnavarrob@ufpso.edu.co>
2024-09-14 07:36:13 -0500

    Arreglar ejercicio poo

---
+++ clasesabado/src/main/java/com/mycompany/clasesabado/Clasesabado.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.clasesabado;
+
+/**
+ *
+ * @author SCIS2-27
+ */
+public class Clasesabado {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
